20f0641b1502e9b9851da0f50dfb646cf409cfb2
roylu <roylu@uw.edu>
2021-03-21 21:33:49 -0700

    update

--- run.py
+++ run.py
@@ -10,8 +10,12 @@ between them. We can see that, as specified by our transition matrix,
 there are no transition between component 1 and 3.
 """
 
+import copy
+import pickle
+import types
 import numpy as np
 from sklearn.utils import check_random_state
+from functools import partial
 
 from hmmlearn import hmm
 from hmmlearn.utils import normalize
@@ -23,9 +27,9 @@ train_dataset, dev_dataset, vocab = get_datasets("./data/agent")
 
 print('vocab size = ', len(vocab))
 
-n_iter = 1
+n_iter = 10
 random_state=42
-n_components = 4
+n_components = 3
 n_features = len(vocab)
 init_first_state_prob = 0.99
 max_num_splits = 100
@@ -39,10 +43,13 @@ startprob[1:] = (1-init_first_state_prob) / (n_components-1)
 
 # The transition matrix, note that there are no transitions possible
 # between component 1 and 3
-transmat = np.array([[0.7, 0.2, 0.0, 0.1],
-                     [0.3, 0.5, 0.2, 0.0],
-                     [0.0, 0.3, 0.5, 0.2],
-                     [0.2, 0.0, 0.2, 0.6]])
+#transmat = np.array([[0.7, 0.2, 0.0, 0.1],
+#                     [0.0, 0.5, 0.2, 0.0],
+#                     [0.0, 0.0, 0.5, 0.2],
+#                     [0.0, 0.0, 0.0, 0.6]])
+transmat = np.array([[0.6, 0.2, 0.2],
+                     [0.0, 0.6, 0.4],
+                     [0.0, 0.0, 1.0],])
 
 
 rs = check_random_state(random_state)
@@ -55,8 +62,7 @@ x_lens = [len(x) for x in xs]
 xs = np.concatenate(xs).reshape(-1, 1)
 
 # Build an HMM instance and set parameters
-model = hmm.MultinomialHMM(n_components=n_components,
-                           init_params="",
+model = hmm.MultinomialHMM(n_components=n_components, init_params="",
                            n_iter=n_iter, verbose=True)
 
 model.startprob_ = startprob
@@ -69,25 +75,51 @@ print("training ...")
 model = model.fit(xs, x_lens)
 
 
-for s in range(max_num_splits):
-    e = entropy(model.emissionprob_)
-    split_idx = np.argmax(e)
-    split_idx = 0
-    n_components += 1
+try:
+    for s in range(max_num_splits):
     
-    startprob = split_state_startprob(model.startprob_, split_idx)
-    transmat = split_state_transmat(model.transmat_, split_idx)
-    emissionprob = split_state_emission(model.emissionprob_, split_idx)
+        # state-splitting
+        n_components += 1
+        e = entropy(model.emissionprob_)
+        split_idx = np.argmax(e)
     
+        startprob, _ = split_state_startprob(model.startprob_, split_idx)
+        transmat, transmat_mask = split_state_transmat(model.transmat_, split_idx)
+        emissionprob, _ = split_state_emission(model.emissionprob_, split_idx)
+        
+        
+        old_model = copy.deepcopy(model)
+        # retrain model again
+        model = hmm.MultinomialHMM(n_components=n_components, init_params="",
+                                   n_iter=n_iter, verbose=True)
     
-    model = hmm.MultinomialHMM(n_components=n_components,
-                               init_params="",
-                               n_iter=1, verbose=True)
-    model._do_mstep = _do_mstep.__get__(model, _do_mstep)
+        _do_mstep = partial(_do_mstep, t_mask=transmat_mask)
+        funcType = types.MethodType
+        model._do_mstep = funcType(_do_mstep, model) 
     
-    model.startprob_ = startprob
-    model.transmat_ = transmat
-    model.emissionprob_ = emissionprob
+        model.startprob_ = startprob
+        model.transmat_ = transmat
+        model.emissionprob_ = emissionprob
+        
+        print(f"training ...")
+        print(f"split_idx = {split_idx}; n_components = {n_components}")
+        
+        model = model.fit(xs, x_lens)
     
-    print("training ...")
-    model = model.fit(xs, x_lens)
+        print(f"last logprob = {old_model.monitor_.history[-1]}")
+        print(f"now logprob = {model.monitor_.history[-1]}")
+    
+        print("*"*50)
+    
+        if model.monitor_.history[-1] < old_model.monitor_.history[-1]:
+            with open(f"{n_components-1}.pkl", "wb") as f:
+                pickle.dump(old_model, f)
+except:
+    print("save old model")
+    with open(f"{n_components-1}.pkl", "wb") as f:
+        pickle.dump(old_model, f)
+    exit()
+
+if model.monitor_.history[-1] > old_model.monitor_.history[-1]:
+    with open(f"{n_components}.pkl", "wb") as f:
+        pickle.dump(old_model, f)
--- sshmm.py
+++ sshmm.py
@@ -1,16 +1,18 @@
 import numpy as np
 from hmmlearn.utils import normalize
+from hmmlearn import hmm
 
-def _do_mstep(self, stats, s_mask=None, t_mask=None):
+def _do_mstep(self, stats, s_mask=None, t_mask=None, e_mask=None):
     if 's' in self.params:
-        startprob_ = np.maximum(self.startprob_prior - 1 + stats['start'],
-                                0)
+        startprob_ = np.maximum(self.startprob_prior - 1 + stats['start'], 0)
         self.startprob_ = np.where(self.startprob_ == 0, 0, startprob_)
         normalize(self.startprob_)
+
     if 't' in self.params:
         transmat_ = np.maximum(self.transmat_prior - 1 + stats['trans'], 0)
         self.transmat_ = np.where(self.transmat_ == 0, 0, transmat_)
         normalize(self.transmat_, axis=1)
+
     if 'e' in self.params:
         self.emissionprob_ = (
             stats['obs'] / stats['obs'].sum(axis=1, keepdims=True))
@@ -23,18 +25,27 @@ def split_state_startprob(old, i):
             p /= 2
         new.append(p)
     new.append(new[i])
-    return np.array(new)
+    new = np.array(new)
+    return new, None
 
 
 def split_state_transmat(old, i):
     new = []
+    mask = []
     for j, p in enumerate(old):
         p = p.tolist()
         if i != len(old) and i == j:
-            p[i+1] /= 2
-            p.append(p[i+1])
+            if i+1 != len(old):
+                p[i+1] /= 2
+                p.append(p[i+1])
+            else:
+                p[i] /= 2
+                p.append(p[i])
+            m = [1]*len(p)
         else:
             p.append(0)
+            m = [0] * (len(p)-1) + [1]
+        mask.append(m)
         new.append(p)
 
     if i == len(old):
@@ -42,17 +53,22 @@ def split_state_transmat(old, i):
         new[i][j] /= 2
 
     new.append(new[i])
+    mask.append(mask[i])
     new = np.array(new)
+    mask = np.array(mask)
+
     new[-1][-1] += new[-1][i]
     new[-1][i] = 0
-    return np.array(new)
+
+    return new, mask
 
 
 def split_state_emission(old, i):
     b = ((old[i-1] + old[i]) / 2).tolist()
     new = old.tolist()
     new.append(b)
-    return np.array(new)
+    new = np.array(new)
+    return new, None
 
 
 def entropy(prob):
