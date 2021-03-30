14bb7d628298ccea27486b852981ec245b58327c
roylu <roylu@uw.edu>
2021-03-29 23:49:40 -0700

    upldate

---
+++ predict.py
@@ -0,0 +1,98 @@
+import sys
+import copy
+import pickle
+import types
+import numpy as np
+import pandas as pd
+from sklearn.utils import check_random_state
+from functools import partial
+from pprint import pprint
+from tqdm import tqdm
+
+from hmmlearn import hmm
+from hmmlearn.utils import normalize
+
+import argparse
+
+from data import get_datasets
+from sshmm import _do_mstep, split_state_startprob, split_state_transmat, split_state_emission, entropy
+
+pd.options.display.max_colwidth = 150
+
+topk_cluster = 30
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--model_path",
+    required=True,
+    type=str,
+    help="trained model path",
+)
+parser.add_argument(
+    "--centroid_path",
+    required=True,
+    type=str,
+    help="csv file: cluster id to centroid utterance",
+)
+parser.add_argument(
+    "--raw_path",
+    required=True,
+    type=str,
+    help="csv file: raw clustering file",
+)
+parser.add_argument(
+    "--output_path",
+    required=True,
+    type=str,
+    help="output file path",
+)
+
+args = parser.parse_args()
+
+train_dataset, dev_dataset, vocab, cnt = get_datasets("./data/kmedoids_agent_150", topk_cluster)
+topk_cluster_ids = list(vocab.keys())
+vocab = {v: k for k, v in vocab.items()}
+print('vocab size = ', len(vocab))
+
+with open(args.model_path, "rb") as f:
+    model = pickle.load(f)
+
+df = pd.read_csv(args.centroid_path)
+raw_df = pd.read_csv(raw_path, compression="gzip")
+
+xs = []
+ids = []
+for x, _id in dev_dataset:
+    xs.append(x)
+    ids.append(_id)
+x_lens = [len(x) for x in xs]
+
+e = model.emissionprob_
+n_states, n_clusters = e.shape
+assert n_clusters == topk_cluster
+
+dfs = []
+for _id, x, x_len in tqdm(zip(ids, xs, x_lens), total=len(xs)):
+    states = model.predict(x.reshape(-1, 1), np.array([x_len]))
+    if len(set(states)) > 1:
+        cluster_seq = []
+        for s in states:
+            #cluster_seq.append(np.random.choice(np.arange(n_clusters), p=e[s]))
+            cluster_seq.append(np.argmax(e[s]))
+
+        utts = []
+        chat = raw_df[raw_df["sourcemediaid"] == _id]
+        chat = chat[chat["cluster"].isin(topk_cluster_ids)]
+        assert len(chat) == len(states)
+
+        ori_utts = chat["phrase"].tolist()
+        ori_clusters = chat["cluster"].tolist()
+
+        for s, c, ou, oc in zip(states, cluster_seq, ori_utts, ori_clusters):
+            c = vocab[c]
+            utt = df[df["cluster"] == c]["phrase"].tolist()[0]
+            utts.append((_id, oc, c, s, utt, ou))
+        dfs.append(pd.DataFrame(utts, columns=["sourcemediaid", "original_cluster", "sampled_cluster", "state", "phrase", "original_phrase"]))
+
+df = pd.concat(dfs)
+df.to_csv(args.output_path, sep="|", index=False)
